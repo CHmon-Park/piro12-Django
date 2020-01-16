7e0359f0318a7fa235369048ddd741ca11fa655f
ChanHyung-Park <pchrpg@naver.com>
2020-01-17 01:12:40 +0900

    5강

---
+++ Askcompany/shop/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+from shop import views
+from django.urls import register_converter
+from . import views
+from .converters import FourDigitYearConverter
+
+register_converter(FourDigitYearConverter, 'yyyy')
+
+app_name = 'shop'
+
+urlpatterns = [
+    path('archives/<yyyy:year>/', views.archives_year),
+]
