e6755176d6ee7245143f977811f820a054d7d851
ChanHyung-Park <pchrpg@naver.com>
2020-01-17 01:35:05 +0900

    6강

--- Askcompany/shop/views.py
+++ Askcompany/shop/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
 # Create your views here.
 
 def archives_year(request, year):
 
     return HttpResponse('{}년도에 대한 내용'.format(year))
+
+def response_pillow_image(request):
+    ttf_path = 'C:/Windows/Fonts/malgun.ttf'
+
+    image_url = 'http://www.flowermeaning.com/flower-pics/Calla-Lily-Meaning.jpg'
+    res = requests.get(image_url)
+    io = BytesIO(res.content)
+    io.seek(0)
+
+    canvas = Image.open(io).convert('RGBA')
+    font = ImageFont.truetype(ttf_path, 40)
+    draw = ImageDraw.Draw(canvas)
+
+    text = 'Ask Company'
+    left, top = 10, 10
+    margin = 10
+    width, height = font.getsize(text)
+    right = left + width + margin
+    bottom = top + height + margin
+    draw.rectangle((left, top, right, bottom), (255, 255, 224))
+    draw.text((15, 15), text, font=font, fill=(20,20,20))
+
+    response = HttpResponse(content_type = 'image/png')
+    canvas.save(response, format='PNG')
+    return response
